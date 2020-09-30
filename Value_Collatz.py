695680ca74ab8b0f96b8ef66a24da94d837a73a0
vlad196 <vlad1.96@mail.ru>
2020-09-30 02:13:49 +0200

    aaaa

---
+++ Value_Collatz.py
@@ -0,0 +1,14 @@
+def collatz(number):
+    while number != 1:
+        print(number)
+        if number % 2 == 0:
+            number = number // 2
+        elif number % 2 == 1:
+            number = 3 * number + 1
+    else:
+        number = 1
+        print(number)
+
+
+print("insert value")
+collatz(int(input()))
---
+++ main.py
@@ -0,0 +1,13 @@
+spam = ""
+while spam !="exit":
+    spam = input()
+    if spam == "1":
+        print("Hello")
+        continue
+    elif spam == "2":
+        print("Greetings")
+        continue
+    else:
+        print("Hi")
+        continue
+#продолжение урока
