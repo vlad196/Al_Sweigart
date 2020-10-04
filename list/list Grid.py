5642e80382a6ab1dc16320dd009f3da0c45dd333
vlad196 <vlad1.96@mail.ru>
2020-10-04 20:29:04 +0200

    Create grid from list

--- list/list Grid.py
+++ list/list Grid.py
@@ -7,8 +7,7 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
-for i in range(6):
-    print(grid[0][i]) #сначала выбираем строку где хотим писать
-    for b in range(9):
-        print(grid[b][i], end="") #каждый раз в одной строке по 8 раз проходимся каждым значением
-        #Error: почему в 1ой строке одна точка а в последней нет?
+for i in range(6): # https://ru.stackoverflow.com/questions/1185961/%d0%a1%d0%b5%d1%82%d0%ba%d0%b0-%d1%81%d0%b8%d0%bc%d0%b2%d0%be%d0%bb%d0%be%d0%b2-%d0%b8%d0%b7-%d1%81%d0%bf%d0%b8%d1%81%d0%ba%d0%b0-%d1%81%d0%bf%d0%b8%d1%81%d0%ba%d0%be%d0%b2-%d0%b2-python-%d0%b7%d0%b0%d0%b4%d0%b0%d1%87%d0%b0-%d1%81%d0%bf%d0%b8%d1%81%d0%ba%d0%be%d0%b2-%d0%b8%d0%b7-%d0%ba%d0%bd%d0%b8%d0%b3%d0%b8-al-sweigart/1185974#1185974
+    for j in range(9):
+        print(grid[j][i], end="")
+    print()
