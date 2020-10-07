4f7c79d072c77f0d9099856c0e3be7e90a8c73bc
vlad196 <vlad1.96@mail.ru>
2020-10-08 01:14:30 +0200

    create dict

---
+++ Dict.py
@@ -0,0 +1,9 @@
+import pprint
+
+Arnold = {"rope":1, "torch":6, "gold coin":42, "dagger":1,"arrow":12}
+def displayinventory(NPC):
+    For i in range(NPC):
+        pprint(str(NPC.keys) + str(NPC.items))
+
+
+displayinventory(Arnold)
