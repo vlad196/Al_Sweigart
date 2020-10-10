eb7a56d0316396b431dce5b767c4d58474cba33f
vlad196 <vlad1.96@mail.ru>
2020-10-10 22:58:26 +0200

    Finally i win this code (just was needed return dictionary from function)

--- Dragonloot.py
+++ Dragonloot.py
@@ -1,21 +1,29 @@
-def displayinventory(NPC):
-    for k, v in NPC.items():
-        print(str(v) + " " + k)
-    print("Total number of items:", sum(NPC.values()))
+def displayinventory(npc):  # Сама функция инвентаря
+    for k, v in npc.items():  # создаём цикл for для переменных key(ключа) и value(значения)
+        # в диапазоне "предметов"(пар слов) словаря
+        print(str(v) + " " + k)  # выводим в виде строки value(значение) + key(ключ, т.е. сам предмет)
+    print("Total number of items:", sum(npc.values()))  # Выводим сумму значений value словаря
 
-def addToInventory(inventory, addedItems):
-    count = {}
-    for items in addedItems:
-        count.setdefault(items, 0)
-        count [items] = count[items] + 1
-    print(count)
-    for k, v in count.items():
-        if k not in inventory:
-            inventory.setdefault(k, 1)
-        elif k in inventory:
-            inventory[k] = inventory[k] + v
 
-inv = {"gold coin" : 42, "rope" : 1}
-dragonLoot = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]
-inv = addToInventory(inv, dragonLoot)
-displayinventory(inv)
+def addtoinventory(inventory, addeditems):  # функция добавления списка лута в словарь инвентаря
+    dictloot = {}  # Сначала создаём пустой словарь для лута
+    for items in addeditems:  # чтобы перенести список в словарь
+        # создаём цикл для переменной items в диапазоне списка
+        dictloot.setdefault(items, 0)  # используем метод .setdefault на пустом словаре.
+        # (items это значения списка, выводящиеся по порядку в диапазоне самого списка addeditems)
+        # Если новый предмет, то добавляем его в словарь и даём значение 0 (там все добавляются)
+        dictloot[items] = dictloot[items] + 1  # Если уже есть пара ключа значения, то добавляем + 1
+    for k, v in dictloot.items():  # Тут добавляем значения одного словаря в другой
+        # создаём цикл с переменными key и value в диапазоне пары ключей значений в словаре dictloot
+        if k not in inventory:  # Если key предмета нет в словаре инвентаря
+            inventory.setdefault(k, 1)  # То добавляем его со значением 1
+        elif k in inventory:  # Илиесли key предмет есть в инвентаре
+            inventory[k] = inventory[k] + v  # То добавляем к !!!определённому!!! в итеррации цикла
+            # key предмету v значение
+    return inventory  # обязательно возвращвем полученный и нужгный нам словарь, иначе будет возвращено none!
+
+
+inv = {"gold coin": 42, "rope": 1}  # Изначально у нас есть инвентарь из словаря
+dragonLoot = ["gold coin", "dagger", "gold coin", "gold coin", "ruby"]  # у нас есть лут из списка
+inv = addtoinventory(inv, dragonLoot)  # Переадресуем переменнную на обновлённый словарь, после функции
+displayinventory(inv)  # Выводим инвентарь на дисплей
--- tests.py
+++ tests.py
@@ -1,18 +1,4 @@
-import math
+addeditems = [228,2,5,22]
 
-def displayInventory(inventory):
-    print('Inventory')
-    for k, v in inventory.items():
-        print(v, k)
-    print('Total number of items:' + ' ' + str(sum(inventory.values())))
-
-
-def addToInventory(inventory, addedItems):
-    for i in range(len(addedItems)):
-        inventory[i] = inventory.get(i, 0) + 1
-    return inventory
-
-inv = {'gold coin':42, 'rope':1}
-dragonLoot = ['gold coin','dagger','gold coin','gold coin','ruby']
-inv = addToInventory(inv, dragonLoot)
-displayInventory(inv)
+for items in addeditems:
+    print(items)
