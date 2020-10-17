37433a8e7fefcb40c60eebf49828f73ce50ab771
vlad196 <vlad1.96@mail.ru>
2020-10-17 23:07:30 +0200

    Printtable

---
+++ PrintTable.py
@@ -0,0 +1,12 @@
+#! python3
+# PrintTable.py - Выводит список в ровной таблице
+
+tabledata = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+
+# manipulating strings 143
+
+def printtable():
+    pass
