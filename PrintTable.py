c685eeab31824e80caff1eb12493a7d69f364ff9
vlad196 <vlad1.96@mail.ru>
2020-10-18 14:57:03 +0200

    Взял решение с stackoverflow, нужно вспомнить про append и изучить sort с его "key="

--- PrintTable.py
+++ PrintTable.py
@@ -8,5 +8,27 @@ tabledata = [['apples', 'oranges', 'cherries', 'banana'],
 
 # manipulating strings 143
 
-def printtable():
-    pass
+'''def printtable(data):
+    for i in range(len(data)):
+        colwights = [0] * len(data)
+        print(colwights)'''
+
+# как должно быть
+
+
+def printtable(data):
+    cols = len(data)
+    rows = len(data[0])
+
+    col_width = []
+    for col in data:
+        col_width.append(len(sorted(col, key=len)[-1]))
+
+    for row in range(rows):
+        result = []
+        for col in range(cols):
+            result.append(data[col][row].rjust(col_width[col]))
+        print('\t'.join(result))
+
+
+printtable(tabledata)
