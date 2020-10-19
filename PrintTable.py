afd5e78b4d8fcef99444acb51712a83fb14f88e0
vlad196 <vlad1.96@mail.ru>
2020-10-19 23:53:55 +0200

    Разобрал чужое решение. Узнал про .sorted Лучше понял for Понял как вытаскивать результать из списка списков

--- PrintTable.py
+++ PrintTable.py
@@ -6,29 +6,28 @@ tabledata = [['apples', 'oranges', 'cherries', 'banana'],
              ['dogs', 'cats', 'moose', 'goose']]
 
 
-# manipulating strings 143
+# manipulating strings 143 для чего это в книжке?...
 
-'''def printtable(data):
-    for i in range(len(data)):
-        colwights = [0] * len(data)
-        print(colwights)'''
+def printtable(data):  # Создаём функцию, где:
+    cols = len(data)  # Число колонок это кол-во объектов в списке data
+    rows = len(data[0])  # Число строк это кол-во объектов в 1-м списке списка data (неуниверсально.
+    # Можно сделать в зависимости от максимума в строке объектов
 
-# как должно быть
+    col_wigth = []  # Новый список, отвечающий за ширину
+    # Цикл for это поочередный перебор объекта (col) из объекта (data).
+    # перебирает разный по весу горох в одном стручке и выдаёт массу каждого гороха по очереди. (как пример)
+    # а если там range(), то это расчёт в диапазоне кол-ва гороха
+    for col in data:  # создаём цикл значений col из data
+        col_wigth.append(len(sorted(col, key=len)[-1]))  # В спискок col_wight вставляем колличество символов
+        # последнего объекта (col) из списка [-1] отсортированного по длинне символов sorted key = len
 
-
-def printtable(data):
-    cols = len(data)
-    rows = len(data[0])
-
-    col_width = []
-    for col in data:
-        col_width.append(len(sorted(col, key=len)[-1]))
-
-    for row in range(rows):
-        result = []
-        for col in range(cols):
-            result.append(data[col][row].rjust(col_width[col]))
-        print('\t'.join(result))
+    for row in range(rows):  # создаём цикл значений row из диапазона значений rows
+        result = []  # Новый список для каждой строки
+        for col in range(cols):  # создаём цикл значений col  диапазоне значений cols
+            result.append(data[col][row].rjust(col_wigth[col]))  # к списку result добавляем значение списка row
+            # из списка col,где справа rjust добавляет пробелы равные значению col из списка col_wight
+        print('\t'.join(result))  # Списки соединяем в строки с горизонтальными табуляциями
+        # (можно было и пробелами но так лучше)
 
 
 printtable(tabledata)
