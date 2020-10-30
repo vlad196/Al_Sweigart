74999dd72e96461f894ac250a243eba4492bd6e1
vlad196 <vlad1.96@mail.ru>
2020-10-30 23:41:41 +0200

    Задал вопрос по регулярным выражениям

--- StatesAndCities.py
+++ StatesAndCities.py
@@ -5,16 +5,25 @@ import pyperclip
 import re
 
 #  Создаём правила регулярных выражений для ключа и значения
-States = re.compile('''
-''')
-
+states = re.compile('''(
+    (?!=[(])
+    [А-Яа-я\s]+)
+    ''', re.VERBOSE)
+cities = re.compile('''(
+  (?<=[(])
+  [А-Яа-я\s]+)''', re.VERBOSE)
 #  Сначала строка копирует строку
+text = str(pyperclip.paste())
+
+print(states.findall(text))
+print(cities.findall(text))
 
 #  Создаём новый словарь
-
+matches = {}
 #  Регулярными выражениями в ключ добавляем города,
 #  а в значение штаты
-
+for statescities in
+#  dict.update...добавлять в виде кортежа
 #  Преобразуем в строку типа словаря
 
 #  копируем
