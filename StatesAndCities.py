3097d6704cef7c3aaf6f97af0f83718c86e3b776
vlad196 <vlad1.96@mail.ru>
2020-10-31 22:03:02 +0200

    Нужно разобрать выборку, почитать про finditer и x.groupdict()

--- StatesAndCities.py
+++ StatesAndCities.py
@@ -5,24 +5,19 @@ import pyperclip
 import re
 
 #  Создаём правила регулярных выражений для ключа и значения
-states = re.compile('''(
-    (?!=[(])
-    [А-Яа-я\s]+)
-    ''', re.VERBOSE)
-cities = re.compile('''(
-  (?<=[(])
-  [А-Яа-я\s]+)''', re.VERBOSE)
+findstatescities = re.compile(r'(?P<state>[^\W\d_]+(?:\s+[^\W\d_]+)*)\s*\((?P<city>[^()]*)\)')
 #  Сначала строка копирует строку
 text = str(pyperclip.paste())
 
-print(states.findall(text))
-print(cities.findall(text))
+statescities = [x.groupdict() for x in re.finditer(findstatescities, text)]
+print(statescities)
+
 
 #  Создаём новый словарь
 matches = {}
 #  Регулярными выражениями в ключ добавляем города,
 #  а в значение штаты
-for statescities in
+
 #  dict.update...добавлять в виде кортежа
 #  Преобразуем в строку типа словаря
 
