c16b39b6c12464eaa356143f24e21b013a689999
vlad1 <vlad1.96@mail.ru>
2022-02-07 22:41:27 +0200

    Просто дз

---
+++ Homework.py
@@ -0,0 +1,11 @@
+#! Python3
+# -*- coding: utf-8 -*-
+#Тут будет простая домашняя работв
+import random
+spam = random.randint(1,3)
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("Howdy")
+elif spam == 3:
+    print("Greetings!")
