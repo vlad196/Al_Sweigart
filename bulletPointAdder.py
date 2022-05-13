7bf019101eaa51b96917674b6ad5c3d51198693a
vlad <vlad1.96@mail.ru>
2022-05-13 21:45:04 +0200

    У линукса проблемы с pyperclip

---
+++ bulletPointAdder.py
@@ -0,0 +1,11 @@
+#! python3
+# coding: utf-8
+
+#bulletPointAdder.py - дбоавляет звёздочку в начле каждой новой строки и возвращает это в буфер обмена
+
+import pyperclip
+
+text = pyperclip.paste
+
+print(text)
+
