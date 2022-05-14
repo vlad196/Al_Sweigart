1793daa20d94cf9d72ddc52533ad5a699b8ad3fe
vlad196 <vlad1.96@mail.ru>
2022-05-14 21:36:36 +0200

    Тут очень важный момент с циклом for. У нас тут берётся i в  числе списка lines?, а уже в самом списке меняется элемент i

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -5,7 +5,12 @@
 
 import pyperclip
 
-text = pyperclip.paste
-
+text = pyperclip.paste()
+# добавление звёздочки с кадой новой строки
+lines = text.split('\n') # Из элемента строки сделал список через \n
+for i in range(len(lines)): # в колличестве элементов списка поочередно выбрать элемент
+    lines[i] = '* ' + lines[i]
+text = '\n'.join(lines) # Соединил список в один элемент через \n
+pyperclip.copy(text)
 print(text)
 
